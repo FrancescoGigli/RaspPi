915d3f91fa7a624ee7b37b11b8db78d1df1e52ec
FrancescoGigli <65950922+FrancescoGigli@users.noreply.github.com>
2020-07-14 12:00:57 +0200

    Update RaspPiMessage.py

--- RaspPiMessage.py
+++ RaspPiMessage.py
@@ -6,6 +6,7 @@ import time
 import requests as req
 
 resp = req.get("http://192.168.1.9:15000//local/VehicleCounter/getDiagnostics.cgi")
+resp.json()
 
 #print(resp.text)
 
@@ -32,8 +33,11 @@ def iothub_client_telemetry_sample_run():
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
 
         while True:
-            print(resp.text)
-            client.send_message(resp.text)
+            # print(resp.text[2,6])
+            string= resp.text
+            print(string[500:520])
+            #client.send_message(resp.text)
+            client.send_message(string)
             print ( "Message successfully sent" )
             time.sleep(10)
             
